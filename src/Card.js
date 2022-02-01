47a07d4e08f2b10d90fd6fcb7940347bc7ba7854
Emily Corcoran <cathleencorcoran@gmail.com>
2022-01-31 19:49:06 -0500

    Build Card Class and passed first 5 tests

--- src/Card.js
+++ src/Card.js
@@ -1,3 +1,12 @@
+class Card {
+  constructor(number, question, answers, correctAnswer) {
+    this.number = number;
+    this.question = question;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+  }
+};
+
 
 
 
