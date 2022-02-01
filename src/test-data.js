c21c5633bfa2a5c349382db257850ee6fc982577
Emily Corcoran <cathleencorcoran@gmail.com>
2022-02-01 18:57:32 -0500

    Add test-data file with Henry info

---
+++ src/test-data.js
@@ -0,0 +1,27 @@
+const testData = [
+  {
+  "id": 1,
+  "question": 'What is Henry\'s favorite foods?',
+  "answers": ['chicken', 'peanutButter', 'apples'],
+  "correctAnswer": 'chicken'
+},
+
+{
+  "id": 2,
+  "question": 'What are Henry\'s favorite toys?',
+  "answers": ['ball', 'rope', 'frisbee'],
+  "correctAnswer": 'ball'
+},
+
+{
+  "id": 3,
+  "question": 'What month was Henry born?'',
+  "answers": ['December', 'January', 'February'],
+  "correctAnswer": 'February'
+}
+
+];
+
+module.exports = {
+  testData
+};
--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,3 +3,18 @@ const expect = chai.expect;
 const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 const Deck = require('../src/Card');
+const data = require('../src/test-data');
+
+
+describe('Deck', () => {
+  let card;
+  let deck;
+
+  beforeEach( () => {
+
+    card = new Card(1, 'What is Henry\'s favorite foods?', ['chicken', 'peanutButter', 'apples'], 'chicken');
+    card2 = new Card(2, 'What are Henry\'s favorite toys?', ['ball', 'rope', 'frisbee'], 'ball');
+    turn1 = new Turn('apples', card1)
+    turn2 = new Turn('ball', card2)
+
+  });
